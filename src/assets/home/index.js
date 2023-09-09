2c018de64996753ee635e9fc513601fb328857e2
amirali taghishah <amirsihezar@hotmail.com>
2023-09-09 13:33:18 +0330

    optimized for test run

---
+++ src/assets/home/index.js
@@ -0,0 +1,14 @@
+import book from './book.svg'
+import children from './children.svg'
+import education from './education.svg'
+import hubArea from './hub_area.svg'
+import notebook from './notebook.svg'
+
+
+export {
+    book,
+    children,
+    education,
+    hubArea,
+    notebook
+}
---
+++ src/assets/instructors-pics/index.js
@@ -0,0 +1,10 @@
+import femaleInstructor from './female.svg'
+import maleInstructor from './male.svg'
+import bookLover from './book-lover.svg'
+
+
+export {
+    femaleInstructor,
+    maleInstructor,
+    bookLover
+}

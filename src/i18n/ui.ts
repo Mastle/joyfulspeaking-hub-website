391a1625919122a025f9eff4ebc4bf217840861c
amirali taghishah <amirsihezar@hotmail.com>
2024-01-09 13:13:54 +0330

    adding persian translation pt.1

---
+++ src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const languages = {
+    en: 'English',
+    pers: 'Persian',
+};
+
+export const defaultLang = 'pers';
+
+export const ui = {
+    pers: {
+        'nav.learn': 'چه چیزی خواهید یادگرفت',
+        'nav.questions': 'سوالات',
+        'nav.blogs': 'بلاگ',
+    },
+    en: {
+        'nav.learn': 'What You\'ll Learn',
+        'nav.questions': 'Questions',
+        'nav.blogs': 'blogs',
+    },
+
+} as const;
